b1f01a134f37cd876085636c33ed0f0f9c88af2e
meenugaur <103583053+meenugaur@users.noreply.github.com>
2022-04-21 15:26:52 +0100

    Add files via upload
    This is my second Maven framework

---
+++ src/test/java/Steps/PostSteps.java
@@ -0,0 +1,30 @@
+package Steps;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+    public class PostSteps {
+        @Given("user should be logged in and should be present at its own wall")
+        public void user_should_be_logged_in_and_should_be_present_at_its_own_wall() {
+            // Write code here that turns the phrase above into concrete actions
+            System.out.println("hello");
+        }
+        @When("I type the massage in the text box")
+        public void i_type_the_massage_in_the_text_box() {
+            // Write code here that turns the phrase above into concrete actions
+            System.out.println("hello");
+        }
+        @When("Click on post button")
+        public void click_on_post_button() {
+            // Write code here that turns the phrase above into concrete actions
+            System.out.println("hello");
+        }
+        @Then("the massage should get posted")
+        public void the_massage_should_get_posted() {
+            // Write code here that turns the phrase above into concrete actions
+            System.out.println("hello");
+        }}
+
+
+
+
